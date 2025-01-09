734a0943b2fbe0c09f50fdba1e39c2114903dd62
TheBigStep <pom51142@gmail.com>
2025-01-09 23:12:07 +0700

    refactor: change variable name from temp to rune like go language

--- main.js
+++ main.js
@@ -4,31 +4,31 @@ fs.readFile(__filename, (_, data) => {
     let content = ""
     let leftBytes = 0
     let firstByte = true
-    let temp = 0
+    let rune = 0
     for (const byte of data) {
         if(firstByte) {
-            if(temp) {
-                content += String.fromCodePoint(temp)
-                temp = 0
+            if(rune) {
+                content += String.fromCodePoint(rune)
+                rune = 0
             }
             if(0x7f | byte === 0x7f) {
-                temp = 0x7f & byte
+                rune = 0x7f & byte
             } else if(0xc0 & byte === 0xc0) {
                 leftBytes = 1
                 firstByte = false
-                temp += (0x1f & byte) * 64 ** leftBytes
+                rune += (0x1f & byte) * 64 ** leftBytes
             } else if(0xe0 & byte === 0xe0) {
                 leftBytes = 2
                 firstByte = false
-                temp += (0x0f & byte) * 64 ** leftBytes
+                rune += (0x0f & byte) * 64 ** leftBytes
             } else {
                 leftBytes = 3
                 firstByte = false
-                temp += (0x07 & byte) * 64 ** leftBytes
+                rune += (0x07 & byte) * 64 ** leftBytes
             }
         } else {
             leftBytes--;
-            temp += (0x3f & byte) * 64 ** leftBytes
+            rune += (0x3f & byte) * 64 ** leftBytes
             if(leftBytes === 0) firstByte = true
         }
     }
